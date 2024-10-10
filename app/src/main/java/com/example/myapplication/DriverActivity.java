7cc43d96d14c6d62a2653e10f6f1490a5c0ecf9d
Christine-Jeep <christinejeep605@gmail.com>
2024-10-10 15:21:09 +0800

    Conductor Ui

--- app/src/main/java/com/example/myapplication/DriverActivity.java
+++ app/src/main/java/com/example/myapplication/DriverActivity.java
@@ -1,24 +1,54 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.myapplication.databinding.ActivityDriverBinding;
 
 public class DriverActivity extends AppCompatActivity {
 
+    ActivityDriverBinding binding;
+
+    @SuppressLint("SuspiciousIndentation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_driver);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityDriverBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());  // Set the layout
+
+        // Set the initial fragment to HomeFragment
+        if (savedInstanceState == null) {
+            replaceFragment(new Driver_HomeFragment()); // Load the HomeFragment by default
+            binding.botnav3.setSelectedItemId(R.id.nav_home3); // Set the home item as selected
+        }
+
+        binding.botnav3.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home3) {
+                replaceFragment(new Driver_HomeFragment());
+            } else if (item.getItemId() == R.id.nav_tracker3) {
+                replaceFragment(new TrackerFragment());
+            } else if (item.getItemId() == R.id.nav_QrCode3) {
+                replaceFragment(new MyQrFragment());
+            } else if (item.getItemId() == R.id.nav_history3) {
+                replaceFragment(new HistoryFragment());
+            } else if (item.getItemId() == R.id.nav_profile3) {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
         });
+    }
+
+    // Method for fragment replacement
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_driver, fragment);
+        fragmentTransaction.commit();
     }
 }
